e6e48ffc89ca7cda9751034cb74479f1d3cec8e0
Abijithgangadharan <111345193+Abijithgangadharan@users.noreply.github.com>
2024-06-02 19:56:11 +0530

    Create cloudwatch.tf

---
+++ terraform/cloudwatch.tf
@@ -0,0 +1,17 @@
+# CloudWatch Log Group for ECS Task Logs
+resource "aws_cloudwatch_log_group" "pt-notication-service" {
+  name              = "/ecs/pt-notication-service"  # Adjust the log group name as needed
+  retention_in_days = 7  # Adjust retention period as needed
+}
+
+# CloudWatch Log Stream for Notification API Service
+resource "aws_cloudwatch_log_stream" "notification_api_log_stream" {
+  name           = "notification-api"
+  log_group_name = aws_cloudwatch_log_group.pt-notication-service.name
+}
+
+# CloudWatch Log Stream for Email Sender Service
+resource "aws_cloudwatch_log_stream" "email_sender_log_stream" {
+  name           = "email-sender"
+  log_group_name = aws_cloudwatch_log_group.pt-notication-service.name
+}
