c2d88307ffee6adf9e5faae3a76945e94745a9fc
Abijithgangadharan <111345193+Abijithgangadharan@users.noreply.github.com>
2024-06-16 15:05:50 +0530

    Update app.controller.spec.ts

--- apps/pt-notification-service/src/app/app.controller.spec.ts
+++ apps/pt-notification-service/src/app/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello PearlThoughts!"', () => {
+      expect(appController.getHello()).toBe('Hello PearlThoughts!');
     });
   });
 });
