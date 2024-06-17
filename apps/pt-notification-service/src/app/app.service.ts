d6b44e53f8dc005794eb6c1229e37a25e2577d63
Abijithgangadharan <111345193+Abijithgangadharan@users.noreply.github.com>
2024-06-17 11:49:25 +0530

    Update app.service.ts

--- apps/pt-notification-service/src/app/app.service.ts
+++ apps/pt-notification-service/src/app/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Vamshi!';
+    return 'Hello Kesari Vamshi Goud!';
   }
 }
